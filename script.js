29ba3a970ce27d7173fd603ff8b2b340218a7808
4x1o <116391629+axiowashere@users.noreply.github.com>
2025-10-06 10:44:33 -0400

    Add user interface
    Players can now see who's turn it is and the
    current score for better tracking. A reset button
    is also included instead of the game automatically
    resetting. A win message will occur if a player
    gets three in a row or ties.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const cards = document.querySelectorAll("div.card")
 console.log(cards);
+const turnIndicator = document.querySelector(".plr-num")
+const p1ScoreIndicator = document.querySelector(".plr1-score")
+const p2ScoreIndicator = document.querySelector(".plr2-score")
 
+const winIndicator = document.querySelector('.win-condition')
+const resetButton = document.querySelector('.reset')
 
 const gameBoard = (function(){
     let board = [['.','.','.'],['.','.','.'],['.','.','.']];
@@ -50,20 +55,39 @@ const gameBoard = (function(){
         return false;
     };
 
-    const visualize = ()=>{
+    const visualize = (turn, p1Score, p2Score)=>{
         for (let y=0; y<3; y++){
             for (let x=0; x<3; x++){
                 const card = cards.item(y*3+x);
                 card.textContent = board[y][x] == '.' ? '' : board[y][x];
+                turnIndicator.textContent = (turn==1) ? ' X' : ' O';
+                p1ScoreIndicator.textContent = p1Score
+                p2ScoreIndicator.textContent = p2Score
             }
         }
     }
-    return {placeX, placeO, wipe, checkWin, visualize}
+
+    const endGame = (winner) => {
+        if (winner == 1){
+            winIndicator.textContent = 'Player X wins!'
+        }else if (winner == 2){
+            winIndicator.textContent = 'Player O wins!'
+        }else if (winner == -1){
+            winIndicator.textContent = 'Tie!'
+        }
+        resetButton.style.display = 'block';
+    }
+
+    return {placeX, placeO, wipe, checkWin, visualize, endGame}
 })();
 
 const gameHandler = (function(){
     let tick = 0;
+    let p1score = 0;
+    let p2score = 0;
+    let gameActive = true;
     const markTile = (num)=>{
+        if (!gameActive) return false;
         const row = Math.floor((num-1)/3);
         const column = (num-1)%3;
         if (tick%2 == 0){
@@ -72,21 +96,42 @@ const gameHandler = (function(){
             if (!gameBoard.placeO(column, row)) return false;
         }   
         if(gameBoard.checkWin() != false){
-            gameBoard.wipe();
-            tick = 0;
+            if (gameBoard.checkWin() == 'o'){
+                p2score++;  
+                gameBoard.endGame(2);
+                gameActive = false;
+            } else if (gameBoard.checkWin() == 'x'){
+                p1score++;
+                gameBoard.endGame(1);
+                gameActive = false;
+            }
+            gameBoard.visualize(tick%2, p1score, p2score);
         };
-        if(tick >= 9){
-            gameBoard.wipe();
-            tick = 0;
+        if(tick >= 8){
+            gameBoard.visualize(tick%2, p1score, p2score);
+            gameBoard.endGame(-1);
+            gameActive = false;
         }
-        gameBoard.visualize();
         tick++;
+        gameBoard.visualize(tick%2+1, p1score, p2score);
     }
-    return {markTile}
+    const reset = ()=>{
+        gameBoard.wipe()
+        tick = 0;
+        gameActive = true;
+        gameBoard.visualize(tick%2+1, p1score, p2score);
+    }
+    return {markTile, reset}
 })();
 
 cards.forEach(node =>{
     node.addEventListener('click',()=>{
         gameHandler.markTile(node.id)
     })
+})
+
+resetButton.addEventListener('click',()=>{
+    gameHandler.reset();
+    resetButton.style.display = 'none';
+    winIndicator.textContent = '';
 })
